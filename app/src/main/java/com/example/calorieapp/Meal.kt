dc98a2cc289d88975c922514093122e2d4e00263
Spagooni <andres.e.escobar@hotmail.com>
2024-10-13 16:12:17 +0800

    New UI

--- app/src/main/java/com/example/calorieapp/Meal.kt
+++ app/src/main/java/com/example/calorieapp/Meal.kt
@@ -3,12 +3,18 @@ package com.example.calorieapp
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-
+/**/
 @Entity(tableName = "meals")
 data class Meal(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val name: String = "",
+    val mealType: String = "",
     @ColumnInfo(name = "meal_calories") val calories: Int = 0,
+    val ingredients: String = "",
+    val totalWeight: Int = 0,
+    val totalProtein: Int = 0,
+    val totalCarbs: Int = 0,
+    val totalFat: Int = 0,
     val photo: ByteArray?,
     var photoUrl: String?
 )
